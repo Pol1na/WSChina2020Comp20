48b5a2107cddde7ae928e79980edd005de408c81
Pol1naHehe <Pol1nameme@yandex.ru>
2021-01-19 20:38:50 +0300

    2.5 finally

--- WSChina2020AppComp20/WSChina2020AppComp20/Pages/AdjustVolunteers.xaml.cs
+++ WSChina2020AppComp20/WSChina2020AppComp20/Pages/AdjustVolunteers.xaml.cs
@@ -20,6 +20,7 @@ namespace WSChina2020AppComp20.Pages
     /// </summary>
     public partial class AdjustVolunteers : Page
     {
+        List<int> volchecked = new List<int>();
         public AdjustVolunteers()
         {
             InitializeComponent();
@@ -27,20 +28,28 @@ namespace WSChina2020AppComp20.Pages
             SkillsNoLess.DisplayMemberPath = "Name";
             SkillsLess.ItemsSource = AppData.Context.Skills.ToList();
             SkillsLess.DisplayMemberPath = "Name";
+
         }
 
         private void ShowBtn_Click(object sender, RoutedEventArgs e)
         {
             LeftDGrid.ItemsSource = AppData.Context.Volunteers.ToList().Where(i => i.Skills.Name == SkillsNoLess.Text);
+            TotalLeftTB.Text = Convert.ToString(LeftDGrid.Items.Count);
+
         }
 
         private void ShowRightBtn_Click(object sender, RoutedEventArgs e)
         {
             RightDGrid.ItemsSource = AppData.Context.Volunteers.ToList().Where(i => i.Skills.Name == SkillsLess.Text);
+            TotalRightTB.Text = Convert.ToString(RightDGrid.Items.Count);
 
         }
 
         private void MoveToRightBtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LeftCheckBox_Checked(object sender, RoutedEventArgs e)
         {
             
         }
